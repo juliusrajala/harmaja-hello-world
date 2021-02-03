36476d930e0c7198206fb2e59bd4be34b05f0af1
Julius Rajala <juliusrajala@gmail.com>
2021-02-03 17:37:21 +0200

    Very basic Hello world with Lonna and Harmaja

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,30 @@
 import { h } from 'harmaja';
+import * as L from 'lonna';
 
-const App = () => <h1>Harmaja</h1>;
+interface State {
+  first: string;
+  second: string;
+}
+
+const state: L.Atom<State> = L.atom<State>({
+  first: 'Hello',
+  second: 'World'
+})
+
+const App = ({ appState = state }) => {
+  const greeting = L.view(appState, 'first');
+  const target = L.view(appState, 'second');
+
+  target.subscribe((value) => console.log('Target value changed', value))
+
+  return (
+    <div>
+      <h1>Harmaja</h1>
+      <button onClick={() => target.set('creator')}>Click me!</button>
+      <h2>{greeting}</h2>
+      <h2>{target}</h2>
+    </div>
+  )
+};
 
 export default App;
